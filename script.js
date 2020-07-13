96cb0c4324b40053797adcd223a9f48c4a528c7d
chavierto <hola@xavierlujan.com>
2020-07-13 16:27:34 -0400

    Target colored squares in DOM, push selection to userMemory

--- script.js
+++ script.js
@@ -17,15 +17,45 @@
 //             if userInput.length === gameMemory.length
 //                 run randomSequence
 
-//Define gameMemory
+//Declare gameMemory, userMemory & turn counter
 const gameMemory = [];
+const userMemory = [];
+let turn = 0;
 
-//Define 20 item random requence
+//Target board and colored squares, declare buttonClick()
+const boardEl = document.querySelector('.board');
+boardEl.addEventListener('click', buttonClick);
+function buttonClick(event) {
+	userMemory.push(event.target.id);
+	console.log(event.target.id);
+}
+
+//Declare 20 item random sequence
 function randomSequence() {
 	for (let i = 0; i < 20; i++) {
 		gameMemory.push(Math.floor(Math.random() * 4));
 	}
-	console.log(gameMemory);
 }
 
 randomSequence();
+
+
+//Declare displaySequence to let user know what to input
+// displaySequence() = for (i=0; i<= turn; i++) {
+//     console.log(gameMemory[i]);
+// }
+
+// displaySequence():
+
+// Define 1st turn
+// function userInput(input) {
+// 	userMemory.push(input);
+// 	for (let i = 0; i <= turn && i < 20; i++) {
+// 		if (userMemory[turn] !== gameMemory[turn]) {
+// 			return 'Game over!';
+// 		} else {
+// 			turn++;
+// 			return 'Nice! continue...';
+// 		}
+// 	}
+// }
