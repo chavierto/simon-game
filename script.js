c72934d022d1961e5dcaeedee4e73d725389a9f6
chavierto <hola@xavierlujan.com>
2020-07-15 00:47:45 -0400

    Start a completely new script file
        -While I had come a long way on the V1 and I was able to determine
         the mechanics of the game, the code was getting too confusing.
        -The main take away is to break things down into smaller pieces,
         I was trying to do too many things at once, this is an attempt at
         refactoring what works and discarding the extraneous code.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//Declare gameMemory, userMemory & turn counter
+//Declare gameMemory, userMemory, turn counter & output counter
 const gameMemory = [];
 const userMemory = [];
 let turn = 0;
+let blinking = 1;
+
+const counterEl = document.querySelector('.counter');
 
 //Declare 20 item random requence
 function randomSequence() {
@@ -15,27 +18,55 @@ const boardEl = document.querySelector('.board');
 boardEl.addEventListener('click', buttonClick);
 function buttonClick(event) {
 	userMemory.push(event.target.id);
+	console.log('click test', userMemory);
+	// if (userMemory.length - 1 == turn) {
 	checkGame();
-	turn++;
-	console.log(event.target.id);
+	// }
 }
 
-//Target start game button
+//Target start game button. Reset gameMemory, userMemory & turn counter
 const buttonEl = document.querySelector('.button');
 buttonEl.addEventListener('click', startGame);
 function startGame() {
 	gameMemory.length = 0;
 	userMemory.length = 0;
 	randomSequence();
-	document.getElementById('gameMemory.[${turn}]');
+	console.clear();
+	repeatSequence(blinking);
 	console.log(gameMemory);
 }
 
 //Declare checkGame function
 function checkGame() {
-	if (userMemory[turn] == gameMemory[turn]) {
+	if (userMemory[userMemory.length - 1] == gameMemory[turn]) {
+		blinking++;
 		console.log('Nice! keep going...');
+		repeatSequence(blinking);
 	} else {
 		console.log('Game over!');
+	}
+	turn++;
+	(counterEl.innerHTML = 'Turn:'), turn;
+	userMemory = [];
+}
+
+//Repeat sequence for user
+function lightUp(index) {
+	return new Promise((resolve, reject) => {
+		document.getElementById(`${gameMemory[index]}`).style.filter =
+			'brightness(300%)';
+		console.log('light up', index);
+		setTimeout(() => {
+			document.getElementById(`${gameMemory[index]}`).style.filter = 'none';
+		}, 500);
+		setTimeout(() => {
+			resolve();
+		}, 500);
+	});
+}
+
+async function repeatSequence(index) {
+	for (let i = 0; i < index; i++) {
+		await lightUp(i);
 	}
 }
